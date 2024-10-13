928aafd4f5692ac4690518ae709bc69acb0f7435
Jhonnathan93 <jsocampod@eafit.edu.co>
2024-10-13 13:14:02 -0500

    Correccion de renovaciones del lease

--- dhcp_server.c
+++ dhcp_server.c
@@ -46,18 +46,16 @@ struct ip_assignment ip_pool[MAX_CLIENTS];
 uint32_t ip_range_start = 0xC0A80064;  // 192.168.0.100 en hexadecimal
 uint32_t ip_range_end = 0xC0A8006E;    // 192.168.0.110 en hexadecimal
 
-// Inicializa el pool de IPs
 void init_ip_pool() {
     for (int i = 0; i < MAX_CLIENTS; i++) {
         ip_pool[i].ip = 0;
         memset(ip_pool[i].mac, 0, 6);
         ip_pool[i].lease_start = 0;
         ip_pool[i].lease_duration = 0;
-        ip_pool[i].xid = 0;  // Inicializa el xid a 0
+        ip_pool[i].xid = 0;
     }
 }
 
-// Encuentra una IP libre
 uint32_t find_free_ip() {
     for (uint32_t ip = ip_range_start; ip <= ip_range_end; ip++) {
         int is_assigned = 0;
@@ -71,96 +69,55 @@ uint32_t find_free_ip() {
             return ip;
         }
     }
-    return 0;  // No hay IPs libres
+    return 0;
 }
 
-// Busca si el cliente ya tiene una IP asignada
 uint32_t find_ip_by_mac(uint8_t *mac) {
     for (int i = 0; i < MAX_CLIENTS; i++) {
         if (memcmp(ip_pool[i].mac, mac, 6) == 0) {
             return ip_pool[i].ip;
         }
     }
-    return 0;  // El cliente no tiene IP asignada
+    return 0;
 }
 
-// Asigna una IP al cliente
 void assign_ip_to_client(uint32_t ip, uint8_t *mac, uint32_t xid) {
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (ip_pool[i].ip == 0) {  // Buscar una entrada libre
+        if (ip_pool[i].ip == 0) {
             ip_pool[i].ip = ip;
             memcpy(ip_pool[i].mac, mac, 6);
-            ip_pool[i].lease_start = time(NULL);  // Tiempo de inicio del arrendamiento
+            ip_pool[i].lease_start = time(NULL);
             ip_pool[i].lease_duration = LEASE_TIME;
-            ip_pool[i].xid = xid;  // Guarda el xid para controlar duplicados
+            ip_pool[i].xid = xid;
             break;
         }
     }
 }
 
-// Libera las IPs cuyos arrendamientos han expirado
 void release_expired_ips() {
     time_t current_time = time(NULL);
     for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (ip_pool[i].ip != 0) {
-            if (difftime(current_time, ip_pool[i].lease_start) > ip_pool[i].lease_duration) {
-                printf("IP %s liberada (lease expirado).\n", inet_ntoa(*(struct in_addr *)&ip_pool[i].ip));
-                ip_pool[i].ip = 0;
-                memset(ip_pool[i].mac, 0, 6);
-                ip_pool[i].lease_start = 0;
-                ip_pool[i].lease_duration = 0;
-                ip_pool[i].xid = 0;  // Limpia el xid
-            }
+        if (ip_pool[i].ip != 0 && difftime(current_time, ip_pool[i].lease_start) > ip_pool[i].lease_duration) {
+            printf("IP %s liberada (lease expirado).\n", inet_ntoa(*(struct in_addr *)&ip_pool[i].ip));
+            ip_pool[i].ip = 0;
+            memset(ip_pool[i].mac, 0, 6);
+            ip_pool[i].lease_start = 0;
+            ip_pool[i].lease_duration = 0;
+            ip_pool[i].xid = 0;
         }
     }
 }
 
-// Construye un DHCP Offer
-void construct_dhcp_offer(struct dhcp_packet *packet, uint32_t offered_ip, uint8_t *mac) {
-    memset(packet, 0, sizeof(struct dhcp_packet));
-
-    packet->op = 2;  // Servidor -> Cliente
-    packet->htype = 1;  // Ethernet
-    packet->hlen = 6;   // Tamaño de la dirección HW
-    packet->hops = 0;
-    packet->xid = htonl(0x12345678);
-    packet->secs = 0;
-    packet->flags = 0;
-    packet->ciaddr = 0;
-    packet->yiaddr = offered_ip;  // IP ofrecida
-    packet->siaddr = inet_addr("192.168.0.1");
-    packet->giaddr = 0;
-    memcpy(packet->chaddr, mac, 6);
-
-    packet->magic_cookie = htonl(DHCP_MAGIC_COOKIE);
-
-    // Opciones DHCP
-    packet->options[0] = 53;
-    packet->options[1] = 1;
-    packet->options[2] = DHCP_OFFER;
-
-    // Máscara de red (opción 1)
-    packet->options[3] = 1;
-    packet->options[4] = 4;
-    uint32_t subnet_mask = inet_addr("255.255.255.0");
-    memcpy(&packet->options[5], &subnet_mask, 4);
-
-    // Puerta de enlace (opción 3)
-    packet->options[9] = 3;
-    packet->options[10] = 4;
-    uint32_t gateway = inet_addr("192.168.0.1");
-    memcpy(&packet->options[11], &gateway, 4);
-
-    // DNS (opción 6)
-    packet->options[15] = 6;
-    packet->options[16] = 4;
-    uint32_t dns_server = inet_addr("8.8.8.8");
-    memcpy(&packet->options[17], &dns_server, 4);
-
-    packet->options[21] = 255;  // Fin de opciones
+void extend_lease_for_client(uint8_t *mac) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (memcmp(ip_pool[i].mac, mac, 6) == 0) {
+            ip_pool[i].lease_start = time(NULL);  // Reiniciar el lease
+            printf("Lease renovado para IP %s.\n", inet_ntoa(*(struct in_addr *)&ip_pool[i].ip));
+            break;
+        }
+    }
 }
 
-// Construye un DHCP ACK
 void construct_dhcp_ack(struct dhcp_packet *packet, uint32_t assigned_ip, uint8_t *mac) {
     memset(packet, 0, sizeof(struct dhcp_packet));
 
@@ -172,50 +129,23 @@ void construct_dhcp_ack(struct dhcp_packet *packet, uint32_t assigned_ip, uint8_
     packet->secs = 0;
     packet->flags = 0;
     packet->ciaddr = 0;
-    packet->yiaddr = assigned_ip;  // IP asignada
+    packet->yiaddr = assigned_ip;
     packet->siaddr = inet_addr("192.168.0.1");
     packet->giaddr = 0;
     memcpy(packet->chaddr, mac, 6);
 
     packet->magic_cookie = htonl(DHCP_MAGIC_COOKIE);
 
-    // Opciones DHCP
     packet->options[0] = 53;
     packet->options[1] = 1;
     packet->options[2] = DHCP_ACK;
     packet->options[3] = 255;
 }
 
-// Construye un DHCP NAK
-void construct_dhcp_nak(struct dhcp_packet *packet, uint8_t *mac) {
-    memset(packet, 0, sizeof(struct dhcp_packet));
-
-    packet->op = 2;
-    packet->htype = 1;
-    packet->hlen = 6;
-    packet->hops = 0;
-    packet->xid = htonl(0x12345678);
-    packet->secs = 0;
-    packet->flags = 0;
-    packet->ciaddr = 0;
-    packet->yiaddr = 0;
-    packet->siaddr = inet_addr("192.168.0.1");
-    packet->giaddr = 0;
-    memcpy(packet->chaddr, mac, 6);
-
-    packet->magic_cookie = htonl(DHCP_MAGIC_COOKIE);
-
-    // Opciones DHCP
-    packet->options[0] = 53;
-    packet->options[1] = 1;
-    packet->options[2] = DHCP_NAK;
-    packet->options[3] = 255;
-}
-
 int main() {
     int sock;
     struct sockaddr_in server_addr, client_addr;
-    struct dhcp_packet dhcp_request, dhcp_ack, dhcp_nak;
+    struct dhcp_packet dhcp_request, dhcp_ack;
     socklen_t client_addr_len = sizeof(client_addr);
     uint8_t client_mac[6];
     uint32_t offered_ip;
@@ -248,38 +178,21 @@ int main() {
             continue;
         }
 
-        printf("DHCP Request recibido del cliente.\n");
+        printf("DHCP Request recibido.\n");
         memcpy(client_mac, dhcp_request.chaddr, 6);
-        xid = ntohl(dhcp_request.xid);  // Extraemos el xid del cliente
+        xid = ntohl(dhcp_request.xid);
 
-        // Verificar si el cliente ya tiene una IP asignada
+        // Verificar si el cliente tiene una IP asignada y si es un request de renovación
         offered_ip = find_ip_by_mac(client_mac);
-        if (offered_ip == 0) {
+        if (offered_ip != 0) {
+            extend_lease_for_client(client_mac);  // Renovar el lease
+        } else {
             offered_ip = find_free_ip();
             if (offered_ip == 0) {
-                printf("No hay más direcciones IP disponibles.\n");
-                construct_dhcp_nak(&dhcp_nak, client_mac);
-                if (sendto(sock, &dhcp_nak, sizeof(dhcp_nak), 0, (struct sockaddr *)&client_addr, client_addr_len) < 0) {
-                    perror("Error al enviar DHCP NAK");
-                } else {
-                    printf("DHCP NAK enviado.\n");
-                }
+                printf("No hay más IPs disponibles.\n");
                 continue;
             }
             assign_ip_to_client(offered_ip, client_mac, xid);
-        } else {
-            // Si el cliente ya tiene IP, verificar si es un duplicado por `xid`
-            struct ip_assignment *client_entry = NULL;
-            for (int i = 0; i < MAX_CLIENTS; i++) {
-                if (memcmp(ip_pool[i].mac, client_mac, 6) == 0) {
-                    client_entry = &ip_pool[i];
-                    break;
-                }
-            }
-            if (client_entry && client_entry->xid == xid) {
-                printf("Solicitud duplicada ignorada (xid = %u).\n", xid);
-                continue;
-            }
         }
 
         construct_dhcp_ack(&dhcp_ack, offered_ip, client_mac);
